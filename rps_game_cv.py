d9f8ddc0d90384eccd31f7d999a16c01d18459d2
rkildea1 <31891933+rkildea1@users.noreply.github.com>
2022-08-18 14:15:38 +0100

    adding a working model with a 10 second timer

--- rps_game_cv.py
+++ rps_game_cv.py
@@ -13,26 +13,24 @@ class CvRps:
     cap = cv2.VideoCapture(0)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     start_time = time.time()
-    seconds = 10
+    seconds = 5
 
     def __init__(self):
         pass
 
-    def get_prediction(self):
-
-        while True: 
+    def get_prediction(self):        
+        
+        ret, frame = CvRps.cap.read()
+        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+        CvRps.data[0] = normalized_image
+        cv2.imshow('frame', frame)
+        prediction = CvRps.model.predict(CvRps.data)
+        while True:
+            # Press q to close the window
             current_time = time.time()
             elapsed_time = current_time - CvRps.start_time
-            ret, frame = CvRps.cap.read()
-            resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-            image_np = np.array(resized_frame)
-            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-            CvRps.data[0] = normalized_image
-            cv2.imshow('frame', frame)
-            prediction = CvRps.model.predict(CvRps.data)
-
-            # Press q to close the window
-            
             if elapsed_time > CvRps.seconds:
                 self.stop_camera()#call the method to stop the camera
                 self.get_user_choice(prediction)#call the convert the array to a choice
@@ -40,6 +38,7 @@ class CvRps:
 
         
     def stop_camera(self):
+        print('1. stopped camera')
         CvRps.cap.release()# After the loop release the cap object
         cv2.destroyAllWindows()# Destroy all the windows
         pass   
@@ -49,12 +48,12 @@ class CvRps:
         idx_max = prediction.argmax() #find the max value
         choices = ["rock", "paper", "scissors", "nothing"]
         user_camera_choice = choices[idx_max]
-        print(f'cv_choice var prints: {user_camera_choice}') #just testing to see if it makes it here
+        print(f'2. cv_choice var prints: {user_camera_choice}') #just testing to see if it makes it here
         self.get_computer_choice(user_camera_choice) 
 
     def get_computer_choice(self,user_choice):
         computer_choice = random.choice(CvRps.choice_list)
-        print(f"You picked: {user_choice}. The computer picked {computer_choice}") #just testing to see if it makes it here
+        print(f"3. You picked: {user_choice}. The computer picked {computer_choice}") #just testing to see if it makes it here
         self.get_winner(user_choice, computer_choice)
 
     def get_winner(self, user_choice, computer_choice):
